ee9945014b38bd393f19910399bbfe72c7f9664d
lap <2171246jo@s.aou.edu.jo>
2022-05-04 08:41:21 -0700

    py

---
+++ strong_password_generator.py
@@ -0,0 +1,14 @@
+# 1- import string module
+# 2- store all characters in lists (upper/lower case, digits, punctuations)
+# 3- take number of characters from user
+# 4- make sure the number of characters is 6 or moore
+# 5- shuffle all lists
+# 6- calculate 30% and 20% of number of characters
+# 7- create password 60% letters and 40% digits and punctuations
+
+import string
+
+s1 = list(string.ascii_lowercase)
+s2 = list(string.ascii_uppercase)
+s3 = list(string.digits)
+s4 = list(string.punctuation)
